b49bad870dc921bfacf049c88a7198e4b8453802
AnriiAndrushko <andry.i.andrushko@gmail.com>
2022-10-19 19:57:22 +0300

    Added Interface +Circle interface +Triangle interface fixed some minor mistype in main class

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -6,13 +6,13 @@ public class Main {
         BaseFactory blackFactory = abstractFactory.getFectory("BlackFactory");
 
         Circle whiteCircle = whiteFactory.createCircle();
-        System.out.println(whiteCircle.preesnt());
+        System.out.println(whiteCircle.present());
         Triangle whiteTriangle = whiteFactory.createTriangle();
-        System.out.println(whiteTriangle.preesnt());
+        System.out.println(whiteTriangle.present());
 
         Circle blackCircle = blackFactory.createCircle();
-        System.out.println(blackCircle.preesnt());
+        System.out.println(blackCircle.present());
         Triangle blackTriangle = blackFactory.createTriangle();
-        System.out.println(blackTriangle.preesnt());
+        System.out.println(blackTriangle.present());
     }
 }
---
+++ Java/src/Triangle.java
@@ -0,0 +1,3 @@
+public interface Triangle {
+    String present();
+}
