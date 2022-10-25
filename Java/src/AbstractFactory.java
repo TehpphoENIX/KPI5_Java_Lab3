fba48fe928c2e344c906bfd8ca876db72a9758c7
DianaTsariuk <115662069+DianaTsariuk@users.noreply.github.com>
2022-10-25 20:54:06 +0300

    Update AbstractFactory.java

--- Java/src/AbstractFactory.java
+++ Java/src/AbstractFactory.java
@@ -1,2 +1,11 @@
 public class AbstractFactory {
+  getFactory(String factories) {
+		switch (factories) {
+		case "WhiteFactory":
+			return new WhiteFactory();
+		case "BlackFactory":
+			return new BlackFactory();
+		default:
+			throw new RuntimeException("ERROR");
+		}
 }
